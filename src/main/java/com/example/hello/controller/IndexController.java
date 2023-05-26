a60e9055d8aa31fac524339afcac6dc8ae5e558c
nehalm4 <93865222+nehalm4@users.noreply.github.com>
2023-05-26 19:09:30 +0530

    Update IndexController.java

--- src/main/java/com/example/hello/controller/IndexController.java
+++ src/main/java/com/example/hello/controller/IndexController.java
@@ -21,7 +21,7 @@ public class IndexController {
 
 	@GetMapping("/list")
 	public List<String> listApi() {
-		List<String> list = Arrays.asList("NEHAL", "GAURI", "SAGAR","SACHIN","MAYUR");
+		List<String> list = Arrays.asList("NEHAL", "GAURI", "SAGAR","SACHIN","MAYUR","PANKAJ");
 		return list;
 	}
 }
